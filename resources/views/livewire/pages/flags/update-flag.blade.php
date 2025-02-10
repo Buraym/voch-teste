c3ac64bf0c57f37be70d2bb4223d47a7b06c3b3f
Brayan Wilis Mauerberg Barbosa <70729051+Buraym@users.noreply.github.com>
2025-02-10 10:06:17 -0300

    refact: add hover preload and refact UI in group, flag and unit update page

--- resources/views/livewire/pages/flags/update-flag.blade.php
+++ resources/views/livewire/pages/flags/update-flag.blade.php
@@ -31,16 +31,30 @@
                 *Campo necessário
             </p>
             @if (count($flag->units) > 0) 
-            <div class="py-2 flex flex-col jsutify-start items center gap-2 mb-4">
-                <p class="mb-2 text-sm text-gray-600 dark:text-gray-400">
-                    Unidades desta bandeira
-                </p>
-                @foreach ($flag->units as $unit)
-                    <a href="{{ route('unit', $unit->id) }}" wire:navigate class="flex justify-center items-center h-10 rounded-lg hover:bg-gray-400 hover:text-gray-900 transition-all dark:bg-white hover">
-                        <i>{{ $unit->name }}</i>
-                    </a>
-                @endforeach
-            </div>
+                <div class="py-2 flex flex-col jsutify-start items center gap-2 mb-4">
+                    <div class="flex justify-start items-center mb-2 gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-fuel text-gray-600 dark:text-gray-400">
+                            <line x1="3" x2="15" y1="22" y2="22"/>
+                            <line x1="4" x2="14" y1="9" y2="9"/>
+                            <path d="M14 22V4a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v18"/>
+                            <path d="M14 13h2a2 2 0 0 1 2 2v2a2 2 0 0 0 2 2a2 2 0 0 0 2-2V9.83a2 2 0 0 0-.59-1.42L18 5"/>
+                        </svg>
+                        <p class="text-sm text-gray-600 dark:text-gray-400 font-bold">
+                            Unidades desta bandeira
+                        </p>
+                    </div>
+                    <div class="px-4 flex flex-col justify-start items-start mb-2 gap-2">
+                        @foreach ($flag->units as $unit)
+                            <a href="{{ route('unit', $unit->id) }}" wire:navigate.hover class="flex gap-2 text-gray-600 dark:text-gray-400">
+                                <i>• {{ $unit->name }}</i>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right">
+                                    <path d="M7 7h10v10"/>
+                                    <path d="M7 17 17 7"/>
+                                </svg>
+                            </a>
+                        @endforeach
+                    </div>
+                </div>
             @endif
             <div class="flex gap-2">
                 <x-primary-button class="max-w-36 flex justify-center text-center">
