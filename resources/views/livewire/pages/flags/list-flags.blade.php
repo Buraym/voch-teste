649980e7653e9fc8092c71010df80b1e3eeeb7ef
Brayan Wilis Mauerberg Barbosa <70729051+Buraym@users.noreply.github.com>
2025-02-10 10:38:12 -0300

    fix: fix search btn in list flags and reports page

--- resources/views/livewire/pages/flags/list-flags.blade.php
+++ resources/views/livewire/pages/flags/list-flags.blade.php
@@ -14,13 +14,13 @@
                 />
                 <x-primary-button class="w-2/6 hidden justify-center text-center sm:flex">
                     Pesquisar
-                </x-danger-button>
+                </x-primary-button>
                 <x-primary-button class="w-14 flex justify-center text-center sm:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search">
                         <circle cx="11" cy="11" r="8"/>
                         <path d="m21 21-4.3-4.3"/>
                     </svg>
-                </x-danger-button>
+                </x-primary-button>
             </div>
         </form>
         <a href="{{ route('add-flag') }}" wire:navigate class="flex justify-center items-center h-10 w-10 rounded-lg dark:bg-gray-900 dark:text-white hover:bg-gray-400 hover:text-gray-900 transition-all hover">
